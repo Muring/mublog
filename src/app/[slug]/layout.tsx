93adb1025cf33e6e1d685bd1b466e00c3fb2814f
MuRing <11esh@naver.com>
2025-04-10 15:01:08 +0900

    refactor: removed trash code

--- src/app/[slug]/layout.tsx
+++ src/app/[slug]/layout.tsx
@@ -1,9 +1,6 @@
-"use client";
-
 import type { ReactNode } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import styled from "@emotion/styled";
 
 export default function PostLayout({ children }: { children: ReactNode }) {
     return (
@@ -14,9 +11,3 @@ export default function PostLayout({ children }: { children: ReactNode }) {
         </div>
     );
 }
-
-// const LayoutWrapper = styled.div`
-//     display: flex;
-//     flex-direction: column;
-//     min-height: 100vh;
-// `;
