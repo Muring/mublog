2f9fd5d293239e9cafb3192ff8bcd4136907fb3a
MuRing <11esh@naver.com>
2025-04-15 11:10:53 +0900

    feat: card component init

---
+++ src/components/PostCard/PostCard.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { CardWrapper } from "./PostCard.styled";
+
+export default function PostCard() {
+    return (
+        <CardWrapper>
+            {/* 이미지 */}
+            {/* 제목 */}
+            {/* 설명 */}
+            {/* 태그 */}
+            <hr />
+            {/* 날짜 */}
+        </CardWrapper>
+    );
+}
