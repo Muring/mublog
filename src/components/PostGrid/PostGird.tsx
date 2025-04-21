38ee99b2fe8b367508d4ede9c536cbfc3b702897
MuRing <11esh@naver.com>
2025-04-21 15:37:30 +0900

    feat: post recent post sort update

--- src/components/PostGrid/PostGird.tsx
+++ src/components/PostGrid/PostGird.tsx
@@ -14,20 +14,24 @@ export default function PostGrid() {
     const searchParams = useSearchParams();
     const selectedTag = searchParams.get("tag");
 
+    // 태그 정렬렬
     const tags = Array.from(new Set(allPosts.flatMap((post) => post.tags ?? []))).sort((a, b) => {
         if (a === "etc") return 1;
         if (b === "etc") return -1;
         return a.localeCompare(b);
     });
 
+    // 포스트 필터링
     const filteredPosts =
         !selectedTag || selectedTag === "all"
             ? allPosts
             : allPosts.filter((post) => post.tags?.includes(selectedTag));
 
+    // 한 번에 보여줄 포스트 개수 6개 정의
     const [visibleCount, setVisibleCount] = useState(BATCH_SIZE);
     const observerRef = useRef<HTMLDivElement | null>(null);
 
+    // Lazy loading 구현
     useEffect(() => {
         const observer = new IntersectionObserver(
             ([entry]) => {
@@ -38,11 +42,16 @@ export default function PostGrid() {
             { threshold: 1 }
         );
 
+        // observeRef에 연결된 div가 보일 때마다 다음 6개 포스트 로딩
         if (observerRef.current) observer.observe(observerRef.current);
         return () => observer.disconnect();
     }, []);
 
-    const visiblePosts = filteredPosts.slice(0, visibleCount);
+    // 검색된 포스트 최신 작성 날짜 순으로 정렬
+    const sortedPosts = [...filteredPosts].sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+    );
+    const visiblePosts = sortedPosts.slice(0, visibleCount);
 
     return (
         <PostGridWrapper>
